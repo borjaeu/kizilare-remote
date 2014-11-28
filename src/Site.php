0772625df66eacb564206e5fb19611d663266537
Borja Morales <borja.morales@softonic.com>
2014-11-28 10:11:37 +0100

    Optional settings

--- src/Site.php
+++ src/Site.php
@@ -3,26 +3,64 @@ namespace Kizilare\Remote;
 
 class Site
 {
+    /**
+     * Number of timeout seconds for connection.
+     *
+     * @var integer
+     */
+    protected $connection_timeout = 5;
+
+    /**
+     * Number of second for process.
+     *
+     * @var integer
+     */
+    protected $timeout = 10;
+
+    /**
+     * User agent sent to the remote server.
+     *
+     * @var string
+     */
+    protected $user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)';
+
     public function getUrl( $url )
     {
         $curl_handler = curl_init( $url );
 
-        // Returns headers with the response also.
         curl_setopt( $curl_handler, CURLOPT_HEADER, 1 );
-
-        // Set the user agent.
-        curl_setopt(
-            $curl_handler,
-            CURLOPT_USERAGENT,
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)'
-        );
-
-        // Get response instead of printing it directly.
+        curl_setopt( $curl_handler, CURLOPT_USERAGENT, $this->user_agent );
+        curl_setopt( $curl_handler, CURLOPT_CONNECTTIMEOUT, $this->connection_timeout );
+        curl_setopt( $curl_handler, CURLOPT_TIMEOUT, $this->timeout );
         curl_setopt( $curl_handler, CURLOPT_RETURNTRANSFER, 1 );
 
         $result = curl_exec( $curl_handler );
 
         return $this->processResult( $result );
+    }
+
+    /**
+     * @param string $user_agent
+     */
+    public function setUserAgent( $user_agent )
+    {
+        $this->user_agent = $user_agent;
+    }
+
+    /**
+     * @param int $timeout
+     */
+    public function setTimeout( $timeout )
+    {
+        $this->timeout = $timeout;
+    }
+
+    /**
+     * @param int $connection_timeout
+     */
+    public function setConnectionTimeout( $connection_timeout )
+    {
+        $this->connection_timeout = $connection_timeout;
     }
 
     protected function processResult( $response )
@@ -40,5 +78,4 @@ class Site
         $data = implode( "\n", $lines );
         return array( 'headers' => $headers, 'response' => $data );
     }
-
 }
